006b85f3778db9cbca3ec5df4ce04db1bc000bf9
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2022-12-25 16:20:56 -0700

    Some interesting floating point findings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+fn finding1() {
+    let mut quotient = 1f64;
+
+    for i in 1..330
+    {
+        quotient /= 10f64;
+        println!("{} {}", i, quotient);
+    }
 }
+
+fn finding2() {
+    let quotient = 1f64;
+
+    for i in 1..64
+    {
+        let denom = 10u128.pow(i);        
+        let q64 = quotient / denom as f64;
+        
+        println!("{} {} {}", i, denom, q64);
+    }
+}
+
+fn main() {
+    // finding1();
+    finding2();
+}
+
+// Findings:
+// 1. There are 323 decimal places (I think) available with f64
+// 2. You get less floating-point trash using / than /=
+//   2a. With /= the fp trash starts at the 6th iteration
+//   2b. With / the fp trash doesn't start until the 23rd iteration
+//   2c. I'm not sure why the denominator calculation is overflowing...
