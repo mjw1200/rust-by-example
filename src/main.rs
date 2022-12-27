c9b9c5039629db307dc4f35b5e23a38cebf0613c
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2022-12-26 19:41:16 -0700

    Oh. You have to list every dependency in Cargo.toml. Ugh.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate rand;
+extern crate rand_chacha;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = ChaCha20Rng::from_entropy();
+    println!("{}", rng.gen_range(0..100));
 }
