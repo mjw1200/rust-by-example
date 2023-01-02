5818194625d17cce929745d6d96260d38ba780aa
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2023-01-01 20:32:26 -0700

    Mostly arrays. Some rand.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::{Rng};
+
 fn main() {
-    println!("Hello, world!");
+    const ELEMENTS:usize = 1000;
+    const HIGH:u32 = 100;
+
+    let mut occurs = [0; HIGH as usize];
+
+    let mut rng = rand::thread_rng();
+
+    for _ in 1..ELEMENTS {
+        let val = rng.gen_range(0..HIGH);
+        occurs[val as usize] += 1;
+    }
+
+    for i in 0..HIGH {
+        println!("{}: {}", i, occurs[i as usize]);
+    }
 }
